7554b6a717fb27055658930d1f2567fc9ff07c6f
anhkien96 <anhkien96@gmail.com>
2016-05-15 00:42:35 +0700

    Updated

---
+++ README.md
@@ -0,0 +1,28 @@
+## FreeCodeCamp API: URL Shortener Microservice
+By Kien Nguyen <anhkien96@gmail.com>
+
+### User stories
+
+1. I can pass a URL as a parameter and I will receive a shortened URL in the JSON response.
+2. If I pass an invalid URL that doesn't follow the valid http://example.com format, the JSON response will contain an error instead.
+3. When I visit that shortened URL, it will redirect me to my original link.
+
+### Example usage
+```
+http://fcc-api-url-shortener.herokuapp.com/new/http://foo.com:80
+http://fcc-api-url-shortener.herokuapp.com/new/http://google.com
+```
+### Example output
+```
+{"original_url":"http://foo.com:80", "short_url":"http://fcc-api-url-shortener.herokuapp.com/SkRv2vXG"}
+```
+### Usage
+```
+http://fcc-api-url-shortener.herokuapp.com/BkpU2PQG
+```
+### Redirect to
+```
+http://google.com
+```
+### Live site
+[http://fcc-api-url-shortener.herokuapp.com](http://fcc-api-url-shortener.herokuapp.com)
---
+++ index.js
@@ -0,0 +1,44 @@
+var mongo = require('mongoose');
+	mongo.connect(process.env.MONGODB_URI);
+	
+var doc = mongo.model('Url', {_id: String, url: String});
+
+require('http').createServer(function(req, res) {
+	var url = req.url;
+	if ( url.length > 1 ) {
+		if (url.substring(1,5) == 'new/') res.end(JSON.stringify(getJson(url.substring(5))));
+		else getDirect(res, url.substring(1));
+	}
+	else getHtml(res);
+})
+.listen(process.env.PORT);
+
+function getDirect(res, id) {
+	doc.find({_id: id}).limit(1).exec(function(err, data) {
+		if (err) throw err;
+		if (data.length > 0) res.writeHead(302, {'Location': data[0].url});
+		else res.write(false);
+		res.end();
+	});
+}
+
+function getJson(url) {
+	if ( isUrl(url) ) {
+		var id = require('shortid').generate();
+		new doc({_id: id, url: url}).save();
+		return {original_url: url, short_url: process.env.APP_URL + id};
+	}
+	else return {error: 'Wrong url format.'};
+}
+
+function getHtml(res) {
+	require('fs').readFile('public/url-shortener.html', function(err, data) {
+		if (err) throw err;
+		res.end(data);
+	});
+}
+
+function isUrl(url) {
+	var path  = url.split('/');
+	return path[0].match(/(https?|ftp):/) && path[1] == '' && path[2].match(/^([a-z0-9-]+\.)+[a-z]{2,7}(:\d+)?$/i);
+}
